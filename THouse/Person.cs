b4ab6826e63ff10abebf6f12258b600333fa4478
ryanpierson87 <ryanpierson87@protonmail.com>
2019-01-18 21:06:26 -0500

    Added the Person class

---
+++ THouse/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace THouse
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public Person()
+        {
+        }
+    }
+}
